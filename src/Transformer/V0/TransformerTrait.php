d4e9bac631a17531d33c1fb1fa514c2bff801112
Jake Noble <jake@hatchedbyyolk.com>
2023-09-15 14:16:12 +0100

    Method for getMappings

--- src/Transformer/V0/TransformerTrait.php
+++ src/Transformer/V0/TransformerTrait.php
@@ -6,12 +6,21 @@ trait TransformerTrait
 {
     public static function transform($permissibleValue)
     {
-        foreach (self::MAPPING as $item) {
-            if(preg_match($item['regex'], $permissibleValue)) {
+        foreach (self::getMappings() as $item) {
+            if (preg_match($item['regex'], $permissibleValue)) {
                 return $item['value'];
             }
         }
 
         return null;
+    }
+
+    public static function getMappings()
+    {
+        if (self::MAPPING) {
+            return self::MAPPING;
+        }
+
+        return [];
     }
 }
