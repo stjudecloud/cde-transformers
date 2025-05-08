b827527a283289ca2a7eef304f2ecf9238a65a93
Jake Noble <hey@jakenoble.me>
2025-05-08 11:44:17 +0100

    feat: added sample id

---
+++ src/CDE/V1/Data/SampleId.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace CCDI\CDE\V1\Data;
+
+use ArrayAccess;
+use CCDI\CDE\Validator\PositiveNumberValidatorTrait;
+
+enum SampleId implements ArrayAccess
+{
+    use PositiveNumberValidatorTrait;
+
+    const CDE_ID = 15100774;
+
+    const URL = 'https://cadsr.cancer.gov/onedata/dmdirect/NIH/NCI/CO/CDEDD?filter=CDEDD.ITEM_ID=15100774%20and%20ver_nr=1.0';
+
+    const DESCRIPTION = 'A unique string of characters used to identify a specimen.';
+
+    const CDE_VERSION = 1;
+
+    // The permissible values for SampleId are a string of characters
+    private const DATA = [];
+}
