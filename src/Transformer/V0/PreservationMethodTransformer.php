101f4535f155bcb82d60911fa8fd8e827e79df9a
Jobin Sunny <Jobin.Sunny@stjude.org>
2026-02-16 16:03:13 -0600

    feat: update preservation method CDE

--- src/Transformer/V0/PreservationMethodTransformer.php
+++ src/Transformer/V0/PreservationMethodTransformer.php
@@ -2,7 +2,7 @@
 
 namespace CCDI\Transformer\V0;
 
-use CCDI\CDE\V1\Data\PreservationMethod;
+use CCDI\CDE\V2\Data\PreservationMethod;
 
 /**
  * @method static string|null|PreservationMethod transform($permissibleValue, $key = null)
@@ -11,20 +11,93 @@ class PreservationMethodTransformer extends AbstractTransformer
 {
     protected static array $MAPPINGS = [
         [
+            'value' => PreservationMethod::MINUS_20_DEGREES_CELSIUS,
+            'regex' => '/^(\-20 degrees c)$/i',
+        ],
+        [
             'value' => PreservationMethod::MINUS_80_DEGREES_CELSIUS,
-            'regex' => '/^(\-80 deg c|-80|-80°)$/i',
+            'regex' => '/^(\-80 degrees c)$/i',
+        ],
+        [
+            'value' => PreservationMethod::CRYOPRESERVED,
+            'regex' => '/^(cryopreserved)$/i',
+        ],
+        [
+            'value' => PreservationMethod::CYTOSPIN_SLIDE,
+            'regex' => '/^(cytospin slide)$/i',
+        ],
+        [
+            'value' => PreservationMethod::EDTA,
+            'regex' => '/^(edta)$/i',
         ],
         [
             'value' => PreservationMethod::FFPE,
             'regex' => '/^(ffpe)$/i',
         ],
         [
+            'value' => PreservationMethod::FORMALIN_FIXED_BUFFERED,
+            'regex' => '/^(formalin fixed - buffered)$/i',
+        ],
+
+        [
+            'value' => PreservationMethod::FORMALIN_FIXED_UNBUFFERED,
+            'regex' => '/^(formalin fixed - unbuffered)$/i',
+        ],
+        [
+            'value' => PreservationMethod::FORMALIN_FIXED_TISSUE,
+            'regex' => '/^(formalin fixed tissue)$/i',
+        ],
+        [
+            'value' => PreservationMethod::FRESH,
+            'regex' => '/^(fresh)$/i',
+        ],
+        [
+            'value' => PreservationMethod::FRESH_DISSOCIATED,
+            'regex' => '/^(fresh dissociated)$/i',
+        ],
+        [
+            'value' => PreservationMethod::FRESH_DISSOCIATED_SINGLE_CELL_SORTED,
+            'regex' => '/^(fresh dissociated and single cell sorted)$/i',
+        ],
+        [
+            'value' => PreservationMethod::FRESH_DISSOCIATED_SINGLE_CELL_SORTED_PLATES,
+            'regex' => '/^(fresh dissociated and single cell sorted into plates)$/i',
+        ],
+        [
             'value' => PreservationMethod::FROZEN,
             'regex' => '/^(frozen)$/i',
         ],
         [
-            'value' => PreservationMethod::LN2,
-            'regex' => '/^(ln2)$/i',
+            'value' => PreservationMethod::ISOPENTANE,
+            'regex' => '/^(isopentane)$/i',
+        ],
+        [
+            'value' => PreservationMethod::LIQUID_NITROGEN,
+            'regex' => '/^(liquid nitrogen)$/i',
+        ],
+        [
+            'value' => PreservationMethod::NOT_APPLICABLE,
+            'regex' => '/^(not applicable)$/i',
+        ],
+        [
+            'value' => PreservationMethod::OCT,
+            'regex' => '/^(oct)$/i',
+        ],
+        [
+            'value' => PreservationMethod::RNALATER,
+            'regex' => '/^(rnalater)$/i',
+        ],
+        [
+            'value' => PreservationMethod::SNAP_FROZEN,
+            'regex' => '/^(snap frozen)$/i',
+        ],
+        [
+            'value' => PreservationMethod::TRIZOL,
+            'regex' => '/^(trizol)$/i',
+        ],
+        [
+            'value' => PreservationMethod::UNKNOWN,
+            'regex' => '/^(unknown)$/i',
         ],
     ];
 }
